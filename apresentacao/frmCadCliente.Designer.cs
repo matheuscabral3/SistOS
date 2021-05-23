368ace36e1e51f8b069a9a59f81732dde9c55b8c
Matheus M <matheusmendescontato81@gmail.com>
2021-05-23 20:26:22 -0300

    commit atualização

--- apresentacao/frmCadCliente.Designer.cs
+++ apresentacao/frmCadCliente.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Location = new System.Drawing.Point(6, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 0;
@@ -69,26 +69,26 @@
             // txbNome
             // 
             this.txbNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbNome.Location = new System.Drawing.Point(94, 26);
+            this.txbNome.Location = new System.Drawing.Point(81, 26);
             this.txbNome.MaxLength = 50;
             this.txbNome.Name = "txbNome";
-            this.txbNome.Size = new System.Drawing.Size(245, 20);
+            this.txbNome.Size = new System.Drawing.Size(256, 20);
             this.txbNome.TabIndex = 1;
             // 
             // txbCidade
             // 
             this.txbCidade.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbCidade.Location = new System.Drawing.Point(94, 61);
+            this.txbCidade.Location = new System.Drawing.Point(81, 61);
             this.txbCidade.MaxLength = 50;
             this.txbCidade.Name = "txbCidade";
-            this.txbCidade.Size = new System.Drawing.Size(245, 20);
+            this.txbCidade.Size = new System.Drawing.Size(256, 20);
             this.txbCidade.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 61);
+            this.label2.Location = new System.Drawing.Point(6, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 17);
             this.label2.TabIndex = 2;
@@ -100,14 +100,14 @@
             this.txbEmail.Location = new System.Drawing.Point(424, 24);
             this.txbEmail.MaxLength = 50;
             this.txbEmail.Name = "txbEmail";
-            this.txbEmail.Size = new System.Drawing.Size(245, 20);
+            this.txbEmail.Size = new System.Drawing.Size(256, 20);
             this.txbEmail.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(346, 24);
+            this.label3.Location = new System.Drawing.Point(361, 24);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 17);
             this.label3.TabIndex = 4;
@@ -116,17 +116,17 @@
             // txbEnd
             // 
             this.txbEnd.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbEnd.Location = new System.Drawing.Point(92, 99);
+            this.txbEnd.Location = new System.Drawing.Point(81, 99);
             this.txbEnd.MaxLength = 50;
             this.txbEnd.Name = "txbEnd";
-            this.txbEnd.Size = new System.Drawing.Size(245, 20);
+            this.txbEnd.Size = new System.Drawing.Size(256, 20);
             this.txbEnd.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(14, 99);
+            this.label4.Location = new System.Drawing.Point(6, 99);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 17);
             this.label4.TabIndex = 6;
@@ -138,14 +138,14 @@
             this.txbTel.Location = new System.Drawing.Point(424, 61);
             this.txbTel.MaxLength = 11;
             this.txbTel.Name = "txbTel";
-            this.txbTel.Size = new System.Drawing.Size(245, 20);
+            this.txbTel.Size = new System.Drawing.Size(256, 20);
             this.txbTel.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(346, 61);
+            this.label5.Location = new System.Drawing.Point(350, 61);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(68, 17);
             this.label5.TabIndex = 8;
@@ -153,7 +153,7 @@
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(94, 222);
+            this.btnCadastrar.Location = new System.Drawing.Point(94, 236);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(90, 30);
             this.btnCadastrar.TabIndex = 10;
@@ -173,7 +173,7 @@
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(238, 222);
+            this.btnConsultar.Location = new System.Drawing.Point(238, 236);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(90, 30);
             this.btnConsultar.TabIndex = 12;
@@ -183,7 +183,7 @@
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(387, 222);
+            this.btnAlterar.Location = new System.Drawing.Point(387, 236);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(90, 30);
             this.btnAlterar.TabIndex = 13;
@@ -192,7 +192,7 @@
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(521, 222);
+            this.btnExcluir.Location = new System.Drawing.Point(521, 236);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(90, 30);
             this.btnExcluir.TabIndex = 14;
@@ -231,14 +231,14 @@
             this.txbNumero.Location = new System.Drawing.Point(424, 99);
             this.txbNumero.MaxLength = 5;
             this.txbNumero.Name = "txbNumero";
-            this.txbNumero.Size = new System.Drawing.Size(245, 20);
+            this.txbNumero.Size = new System.Drawing.Size(256, 20);
             this.txbNumero.TabIndex = 18;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(346, 99);
+            this.label6.Location = new System.Drawing.Point(353, 99);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 17);
             this.label6.TabIndex = 17;
@@ -247,17 +247,17 @@
             // txbBairro
             // 
             this.txbBairro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbBairro.Location = new System.Drawing.Point(92, 137);
+            this.txbBairro.Location = new System.Drawing.Point(81, 137);
             this.txbBairro.MaxLength = 50;
             this.txbBairro.Name = "txbBairro";
-            this.txbBairro.Size = new System.Drawing.Size(245, 20);
+            this.txbBairro.Size = new System.Drawing.Size(256, 20);
             this.txbBairro.TabIndex = 20;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(14, 137);
+            this.label7.Location = new System.Drawing.Point(6, 140);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(51, 17);
             this.label7.TabIndex = 19;
@@ -267,7 +267,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(355, 140);
+            this.label8.Location = new System.Drawing.Point(361, 140);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 17);
             this.label8.TabIndex = 21;
@@ -276,17 +276,17 @@
             // txbRG
             // 
             this.txbRG.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbRG.Location = new System.Drawing.Point(92, 181);
+            this.txbRG.Location = new System.Drawing.Point(81, 181);
             this.txbRG.MaxLength = 10;
             this.txbRG.Name = "txbRG";
-            this.txbRG.Size = new System.Drawing.Size(245, 20);
+            this.txbRG.Size = new System.Drawing.Size(256, 20);
             this.txbRG.TabIndex = 24;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(28, 181);
+            this.label9.Location = new System.Drawing.Point(6, 181);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(35, 17);
             this.label9.TabIndex = 23;
@@ -298,7 +298,7 @@
             this.txbCPF.Location = new System.Drawing.Point(424, 140);
             this.txbCPF.MaxLength = 11;
             this.txbCPF.Name = "txbCPF";
-            this.txbCPF.Size = new System.Drawing.Size(245, 20);
+            this.txbCPF.Size = new System.Drawing.Size(256, 20);
             this.txbCPF.TabIndex = 25;
             // 
             // frmCadCliente
