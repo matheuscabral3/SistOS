3b180a2423b671fdc63e5c10bc7831f0797d362d
Matheus M <matheusmendescontato81@gmail.com>
2021-06-01 09:28:35 -0300

    Equipamentos finalizado

--- apresentacao/frmCadEquipamento.Designer.cs
+++ apresentacao/frmCadEquipamento.Designer.cs
@@ -50,68 +50,62 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(92, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(69, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.Size = new System.Drawing.Size(141, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome do Equipamento:";
             // 
             // txbNomeEquip
             // 
             this.txbNomeEquip.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbNomeEquip.Location = new System.Drawing.Point(96, 78);
-            this.txbNomeEquip.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbNomeEquip.Location = new System.Drawing.Point(72, 63);
             this.txbNomeEquip.Name = "txbNomeEquip";
-            this.txbNomeEquip.Size = new System.Drawing.Size(331, 22);
+            this.txbNomeEquip.Size = new System.Drawing.Size(249, 20);
             this.txbNomeEquip.TabIndex = 1;
             // 
             // txbModelo
             // 
             this.txbModelo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbModelo.Location = new System.Drawing.Point(455, 78);
-            this.txbModelo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbModelo.Location = new System.Drawing.Point(341, 63);
             this.txbModelo.Name = "txbModelo";
-            this.txbModelo.Size = new System.Drawing.Size(331, 22);
+            this.txbModelo.Size = new System.Drawing.Size(249, 20);
             this.txbModelo.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(451, 55);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(338, 45);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tipo:";
             // 
             // txbValor
             // 
             this.txbValor.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbValor.Location = new System.Drawing.Point(96, 145);
-            this.txbValor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbValor.Location = new System.Drawing.Point(72, 118);
+            this.txbValor.MaxLength = 10;
             this.txbValor.Name = "txbValor";
-            this.txbValor.Size = new System.Drawing.Size(331, 22);
+            this.txbValor.Size = new System.Drawing.Size(249, 20);
             this.txbValor.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(92, 123);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(69, 100);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.Size = new System.Drawing.Size(71, 15);
             this.label3.TabIndex = 4;
             this.label3.Text = "Valor Peça:";
             // 
             // btnCadEquip
             // 
-            this.btnCadEquip.Location = new System.Drawing.Point(276, 223);
-            this.btnCadEquip.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCadEquip.Location = new System.Drawing.Point(207, 181);
             this.btnCadEquip.Name = "btnCadEquip";
-            this.btnCadEquip.Size = new System.Drawing.Size(120, 37);
+            this.btnCadEquip.Size = new System.Drawing.Size(90, 30);
             this.btnCadEquip.TabIndex = 6;
             this.btnCadEquip.Text = "Incluir";
             this.btnCadEquip.UseVisualStyleBackColor = true;
@@ -119,10 +113,9 @@
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(512, 500);
-            this.btnVoltar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVoltar.Location = new System.Drawing.Point(384, 406);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(120, 37);
+            this.btnVoltar.Size = new System.Drawing.Size(90, 30);
             this.btnVoltar.TabIndex = 7;
             this.btnVoltar.Text = "Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
@@ -131,40 +124,36 @@
             // txbEstoqueDisp
             // 
             this.txbEstoqueDisp.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txbEstoqueDisp.Location = new System.Drawing.Point(455, 145);
-            this.txbEstoqueDisp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbEstoqueDisp.Location = new System.Drawing.Point(341, 118);
             this.txbEstoqueDisp.Name = "txbEstoqueDisp";
-            this.txbEstoqueDisp.Size = new System.Drawing.Size(331, 22);
+            this.txbEstoqueDisp.Size = new System.Drawing.Size(249, 20);
             this.txbEstoqueDisp.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(451, 123);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(338, 100);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.Size = new System.Drawing.Size(121, 15);
             this.label4.TabIndex = 8;
             this.label4.Text = "Estoque Disponível:";
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(479, 223);
-            this.btnAlterar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAlterar.Location = new System.Drawing.Point(359, 181);
             this.btnAlterar.Name = "btnAlterar";
-            this.btnAlterar.Size = new System.Drawing.Size(120, 37);
+            this.btnAlterar.Size = new System.Drawing.Size(90, 30);
             this.btnAlterar.TabIndex = 10;
             this.btnAlterar.Text = "Alterar";
             this.btnAlterar.UseVisualStyleBackColor = true;
-            this.btnAlterar.Click += new System.EventHandler(this.button1_Click);
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click_1);
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(667, 223);
-            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExcluir.Location = new System.Drawing.Point(500, 181);
             this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(120, 37);
+            this.btnExcluir.Size = new System.Drawing.Size(90, 30);
             this.btnExcluir.TabIndex = 11;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
@@ -172,10 +161,9 @@
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(96, 223);
-            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConsultar.Location = new System.Drawing.Point(72, 181);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(120, 37);
+            this.btnConsultar.Size = new System.Drawing.Size(90, 30);
             this.btnConsultar.TabIndex = 12;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
@@ -190,23 +178,22 @@
             this.dtEquipamentos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dtEquipamentos.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.dtEquipamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtEquipamentos.Location = new System.Drawing.Point(79, 278);
-            this.dtEquipamentos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtEquipamentos.Location = new System.Drawing.Point(59, 226);
+            this.dtEquipamentos.Margin = new System.Windows.Forms.Padding(2);
             this.dtEquipamentos.MultiSelect = false;
             this.dtEquipamentos.Name = "dtEquipamentos";
             this.dtEquipamentos.ReadOnly = true;
             this.dtEquipamentos.RowHeadersWidth = 51;
             this.dtEquipamentos.RowTemplate.Height = 24;
-            this.dtEquipamentos.Size = new System.Drawing.Size(737, 192);
+            this.dtEquipamentos.Size = new System.Drawing.Size(553, 156);
             this.dtEquipamentos.TabIndex = 13;
             this.dtEquipamentos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtEquipamentos_CellContentClick);
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Location = new System.Drawing.Point(233, 500);
-            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLimpar.Location = new System.Drawing.Point(175, 406);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(120, 37);
+            this.btnLimpar.Size = new System.Drawing.Size(90, 30);
             this.btnLimpar.TabIndex = 14;
             this.btnLimpar.Text = "Limpar Tabela";
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -214,10 +201,10 @@
             // 
             // frmCadEquipamento
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(895, 551);
+            this.ClientSize = new System.Drawing.Size(671, 448);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.dtEquipamentos);
             this.Controls.Add(this.btnConsultar);
@@ -234,7 +221,6 @@
             this.Controls.Add(this.txbNomeEquip);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmCadEquipamento";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
